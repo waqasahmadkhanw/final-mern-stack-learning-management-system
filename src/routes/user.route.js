2764866e3f1b7652734447d6afaf0ecd1714f178
waqasahmadkhanw <https://github.com/waqasahmadkhanw/MERN-STACK.git>
2026-02-26 02:14:51 -0800

    routes for courses

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { changeCurrentPassword, getCurrentUser, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails } from "../controllers/user.controller.js";
-import authUser from "../middlewares/auth.middleware.js";
+import authUser, { authorizeRoles } from "../middlewares/auth.middleware.js";
 const router=Router()
 router.route("/register").post(registerUser)
 router.route("/login-user").post(loginUser)
@@ -9,4 +9,16 @@ router.route("/current-user").get(authUser,getCurrentUser)
 router.route("/change-password").patch(authUser,changeCurrentPassword)
 router.route("/update-user-details").patch(authUser,updateAccountDetails)
 router.route("/logout-user").post(authUser,logoutUser)
+//==========course routes===========//
+import { createCourse, deleteCourse, getAllCourses, getSingleCourse, updateCourse 
+} from "../controllers/course.controller.js";
+//------public routes-------//
+router.route("/getall-courses").get(getAllCourses)
+router.route("/:id").get(getSingleCourse)
+//======INSTRUCTOR ROUTES======//
+router.route("/create_course").post(authorizeRoles("instructor"),createCourse)
+router.route("/:id").put(authorizeRoles("instructor"),updateCourse)
+//====== INSTRUCTOR OR ADMIN ROUTES=====//
+router.route("/delete-course").put(authUser,authorizeRoles("instructor", "admin"),deleteCourse)
+
 export default router
